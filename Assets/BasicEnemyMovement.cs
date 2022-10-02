7a0be7ddf25e2eb3e06c82e9913a82c9569cba18
BuildTools <kgauckac@gmx.de>
2022-10-02 12:55:30 +0200

    EnemyRayCasting

--- Assets/BasicEnemyMovement.cs
+++ Assets/BasicEnemyMovement.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BasicEnemyMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
-           
+        // Cast different Rays
+        for (int i = 0; i < 20; i++) 
+        {
+            double angle = (Math.PI / 21.0) * (i+1);
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle)));
+
+            if (hit.collider == null)
+                continue;
+
+            
+        }
     }
 }
