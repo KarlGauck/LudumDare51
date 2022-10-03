8b168ee728b95470bde8c2024b3ea5e54b79ebac
Jonas Heim <jonasbenedictheim@gmail.com>
2022-10-03 14:18:09 +0200

    Dash

--- Assets/Ability.cs
+++ Assets/Ability.cs
@@ -11,16 +11,25 @@ public class Ability : MonoBehaviour
 
     
     public IDictionary<string, bool> abilities = new Dictionary<string, bool>();
-
+    public IDictionary<string, string> abilityClass = new Dictionary<string, string>();
+    Tuple<string, string, int> vorherig = Tuple.Create(" ", " ", 0);
 
     void Start()
     {
-        //abilities.Add("Gun",true);
+        abilities.Add("Gun",true);
         abilities.Add("Doublejump",true);
         abilities.Add("Run",true);
-        //abilities.Add("Superjump",true);
+        abilities.Add("Superjump",true);
         abilities.Add("Rocketlauncher",true);
-        //abilities.Add("Grenade",true);
+        abilities.Add("Grenade",true);
+        abilities.Add("Dash",true);
+        abilityClass.Add("Gun","weapon");
+        abilityClass.Add("Rocketlauncher","weapon");
+        abilityClass.Add("Grenade","weapon");
+        abilityClass.Add("Doublejump","movement");
+        abilityClass.Add("Superjump","movement");
+        abilityClass.Add("Run","movement");
+        abilityClass.Add("Dash","movement");
     }
 
     // Update is called once per frame
@@ -36,16 +45,21 @@ public class Ability : MonoBehaviour
                     keys2.Add(key);
                 }
                 foreach(var  key in keys2){
+                    if(abilities[key]){
+                        int anzahl = 0;
+                        if(abilityClass[key] == vorherig.Item2){
+                            anzahl = vorherig.Item3 + 1;
+                        }
+                        vorherig = Tuple.Create(key, abilityClass[key], anzahl);
+                    }
                     abilities[key] = false;
-                    print(key);
                 }
 
                 // eine zufällige
-                System.Random rd = new System.Random();
-                int rand_num = rd.Next(0,keys2.Count);
-                abilities[keys2[rand_num]] = true;
+                string akey = randomAbility(keys2);
+                abilities[akey] = true;
                 print(true);
-                print(keys2[rand_num]);
+                print(akey);
                 
                 nextAbilitytime = Time.time + everyXseconds;
             }
@@ -62,7 +76,22 @@ public class Ability : MonoBehaviour
         
     }
 
-        
+    string randomAbility(List<string> keys){
+        string key = " ";
+        System.Random rd = new System.Random();
+        for(int i = 0; i < 10; i++){
+            int rand_num = rd.Next(0,keys.Count);
+            key = keys[rand_num];
+            if(vorherig.Item2 != abilityClass[key] ||  vorherig.Item3 < 2){
+                if(vorherig.Item1 != key){
+                    return key;
+                }
+            }
+        }
+        return key;
+            
+    }
+
             // Gun,
             // Doublejump,
             // Superjump,
--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,6 +11,13 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed = 40;
     public float runFaster = 70;
 
+    float cooldown = 2f;
+    float nextActivatetime = 0f;
+    float activatetime = 0f;
+    public float dashTime = 0.1f;
+    public float cooldownDash = 5f;
+    public float dash = 10;
+
     float horizontalMove = 0;
     bool jump = false;
     bool crouch = false;
@@ -27,7 +34,24 @@ public class PlayerMovement : MonoBehaviour
         }else{
             horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
         }
-        
+
+        if(ability.isAktive("Dash")){
+			cooldown = cooldownDash;
+        }
+
+        if(ability.isAktive("Dash")){
+            if(Time.time >= nextActivatetime){
+                if(Input.GetButtonDown("Fire1")){
+                    print("2");
+                    horizontalMove *= dash;
+                    nextActivatetime = Time.time + cooldown;
+                    activatetime = Time.time + dashTime;
+                }
+            }else if(Time.time <= activatetime){
+                horizontalMove *= dash;
+            }
+        }
+
         if(Input.GetButtonDown("Jump")){
             jump = true;
         }
